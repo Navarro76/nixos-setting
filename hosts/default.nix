75f2d5c57fc4ffdad71e50cf33113045655b168b
Navarro76 <alexnm76@gmail.com>
2025-02-11 12:46:57 -0600

    Update default.nix

--- hosts/default.nix
+++ hosts/default.nix
@@ -3,15 +3,15 @@
 {
   imports = [
     ./hardware-configuration.nix    # Configuración de hardware
-    ./modules/system.nix            # Timezone, locales, etc.
-    ./modules/bspwm.nix             # Configuración gráfica (bspwm, polybar, etc.)
-    ./modules/networking.nix        # Configuración de red
-    ./modules/user-alex.nix         # Configuración del usuario alex
-    ./modules/system-packages.nix   # Paquetes del sistema
-    ./modules/docker.nix            # Configuración de Docker
-    ./modules/samba.nix             # Configuración de Samba
-    ./modules/ssh.nix               # Configuración de SSH
-    ./modules/firewall.nix          # Configuración de Samba
+    ../modules/system.nix            # Timezone, locales, etc.
+    ../modules/bspwm.nix             # Configuración gráfica (bspwm, polybar, etc.)
+    ../modules/networking.nix        # Configuración de red
+    ../modules/user-alex.nix         # Configuración del usuario alex
+    ../modules/system-packages.nix   # Paquetes del sistema
+    ../modules/docker.nix            # Configuración de Docker
+    ../modules/samba.nix             # Configuración de Samba
+    ../modules/ssh.nix               # Configuración de SSH
+    ../modules/firewall.nix          # Configuración de Samba
     ../modules/hardware/amd.nix     # Importar configuración de AMD
   ];
 
