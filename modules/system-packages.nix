90ebe3f09377ecbbd2a344d575a019a6299ba907
Navarro76 <alexnm76@gmail.com>
2025-02-11 08:58:03 -0600

    Create system-packages.nix

---
+++ modules/system-packages.nix
@@ -0,0 +1,29 @@
+{ config, pkgs, ... }:
+
+{
+  environment.systemPackages = with pkgs; [
+    pipewire  # Servidor de audio
+    mpd       # Music Player Daemon
+    spaceFM   # Administrador de archivos ligero
+    feh       # Visualizador de imágenes
+  ];
+
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    pulse.enable = true;
+  };
+
+  services.mpd = {
+    enable = true;
+    musicDirectory = "/home/alex/Music";  # Cambia esto según tu estructura
+    network.listenAddress = "localhost";  # Asegura que MPD solo escuche en localhost
+    #network.listenAddress = "127.0.0.1";
+    extraConfig = ''
+      audio_output {
+        type "pulse"
+        name "MPD PulseAudio"
+      }
+    '';
+  }; 
+}
