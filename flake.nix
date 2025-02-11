124a9cd2891363e2093b3313e2b804220f4c53b9
Navarro76 <alexnm76@gmail.com>
2025-02-11 12:47:56 -0600

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,17 +1,17 @@
 {
-  description = "Configuración de NixOS con flakes";
+  description = "Configuración de NixOS con Flakes";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, nixpkgs, home-manager, ... }@inputs: {
-    nixosConfigurations.alex = nixpkgs.lib.nixosSystem {
+    nixosConfigurations.default = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
-        ./hosts/default.nix  # <--- Importamos el archivo del host
+        ./hosts/default.nix
         home-manager.nixosModules.home-manager
         {
           home-manager.useGlobalPkgs = true;
