3c68520d488a959b95c6878b621a095846d13c0f
Navarro76 <alexnm76@gmail.com>
2025-02-11 09:08:40 -0600

    Create alex.nix

---
+++ home-manager/alex.nix
@@ -0,0 +1,33 @@
+{
+  home.username = "alex";
+  home.homeDirectory = "/home/alex";
+
+  # 1. Instalar paquetes personales (pero sin gestionar configuración en Home Manager)
+  home.packages = with pkgs; [
+    neovim
+    git
+    chezmoi       # Manejo de dotfiles
+    calibre       # Lector de ebooks
+    ncmpcpp       # Cliente de MPD
+    zsh           # Shell
+    zsh-autosuggestions
+    zsh-syntax-highlighting
+    fast-syntax-highlighting
+    powerlevel10k
+    tmux
+    navi
+    amule
+    onlyoffice-bin
+    smplayer
+    koodo-reader
+    nerdfonts     # Fuentes necesarias para Powerlevel10k
+  ];
+
+  # 2. NO configurar Zsh en Home Manager (Chezmoi lo manejará)
+
+  # 3. Habilitar Home Manager
+  programs.home-manager.enable = true;
+
+  # 4. Definir la versión de Home Manager
+  home.stateVersion = "23.11";
+}
